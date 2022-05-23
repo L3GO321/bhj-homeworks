c68d0cdd677397d999da154aa70f260193f1894c
L3GO321 <googlik701@gmail.com>
2022-05-23 12:45:59 +0300

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,30 @@
+const taskList = document.getElementById('tasks__list');
+const addButton = document.getElementById('tasks__add');
+const input = document.getElementById('task__input');
+
+const addTask = () => {
+    const task = input.value;
+
+    taskList.insertAdjacentHTML('beforeEnd', `<div class="task">
+    <div class="task__title">
+        ${task}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+</div>`);
+
+    input.value = '';
+    const taskRemove = taskList.querySelector('.task:last-child .task__remove');
+    taskRemove.addEventListener('click', (e) => removeTask(e));
+}
+
+const removeTask = (e) => {
+    taskList.removeChild(e.target.parentNode);
+}
+
+addButton.addEventListener('click', addTask);
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      addTask();
+    }
+});
