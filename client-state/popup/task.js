c376223e1f454633bc0a37b3c32dc7ccc7f13d97
L3GO321 <googlik701@gmail.com>
2022-06-05 11:47:30 +0300

    initial commit

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,7 +2,9 @@ const modal = document.getElementById('subscribe-modal');
 
 const modalClose = document.querySelector('.modal__close');
 
-if (!(document.cookie.indexOf('isClose=true') + 1)) modal.classList.add('modal_active');
+if (!(document.cookie.includes('isClose=true'))) {
+    modal.classList.add('modal_active');
+}
 
 modalClose.addEventListener('click', () => {
     modal.classList.remove('modal_active');
