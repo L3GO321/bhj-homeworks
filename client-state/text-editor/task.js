fa1993b3a0192985b54a25ad9ae416543aba9987
L3GO321 <googlik701@gmail.com>
2022-06-05 11:49:56 +0300

    Initial ommit

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,8 @@
 const editor = document.getElementById('editor');
 
 const storedName = localStorage.getItem('text');
-if (storedName) {
-    editor.textContent = storedName;
-};
+
+editor.textContent = storedName;
 
 editor.addEventListener('input', () => {
     localStorage.setItem('text', editor.value);
